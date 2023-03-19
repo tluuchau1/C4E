7c5eaa34433ac116eacf0520c1e7645d5011a3bc
tluuchau1 <tluuchau1@gmail.com>
2023-03-19 19:30:15 +0700

    Create app.js

---
+++ lesson6/app.js
@@ -0,0 +1,49 @@
+// console.log("welcome to javascript");
+// console.log("hello");
+// console.log("this is test text ");
+
+// console.error("Day la 1 loi");
+// console.warn("đây là cảnh báo");
+
+// tên biến chỉ bắt đầu bằng chữ cái hoặc  "_", hoặc  "$"
+// trong đặt tên biến: có thể dùng số
+// let name;
+// name = "Giá trị mới của name"; // gán giá trị cho biến
+// name = "hello , this is something";
+// console.log(name); // hiện thị giá trị của biến name lên màn hình console
+
+// let age = 10; // khai báo & gán giá trị cho biến
+
+// const hello =" Cần lao vi tiên thủ"; // hằng số: không thể thay đổi giá trị
+
+// console.log(hello); // khi mà đặt tên biến bị lỗi nó sẽ hiện thị dáu gạch chân màu đỏ ở dưới 
+// kiểu số (number)
+let age = 28;
+let weight = 71;
+let height = 170 ;
+let breast1 = 90;
+
+//kiểu chuỗi (string)
+let slogan = "cần lao vi tiên thủ , vô vi thực ... ";
+let name = "Chinh Do";
+let address= "thai binh";
+let phone = "+84 98765683";
+
+// kiểu boolean ()
+let hasGirlfriend = false;
+let isHandsome = true;
+let isRich = false;
+
+// Kiểu Null
+let girlFriend = null;
+let money = null;
+
+// Kiểu undefined 
+
+let future ;
+let wife;
+
+let a = 10; // toán tử so sánh cho ra gia trị boolean: true hoặc false;
+let b ='10';
+
+console.log(a == b);
