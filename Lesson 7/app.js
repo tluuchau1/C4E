bf3b1d0dde9d9996a77818261b095b49c1b204cf
tluuchau1 <tluuchau1@gmail.com>
2023-03-20 23:15:29 +0700

    Create app.js

---
+++ Lesson 7/app.js
@@ -0,0 +1,126 @@
+// let a = "hello";
+// let b =  10;
+
+// nhập dữ liệu từ bàn phím và gán giá trị vừa nhập vào trong 1 biến 
+
+// let input = prompt("Hãy nhập vào đây để gán giá trị cho biến 'input': "); // dữ liệu có kiểu string
+
+// console.log('bạn vừa nhập vào' + input);
+
+// let x = Number(prompt("nhập số x"));
+// let y = Number(prompt("nhập số y"));
+
+// let z = x + y;
+// console.log(z);
+
+// console.log(input);
+
+// let name =" hoang long ";
+// {
+//     let age = 28; // age chỉ tồn tại trong biến 
+//     let name =" someone else";
+//     {
+//         let address = "thai binh";
+//         // address chỉ tồn tại ở trong đây 
+//     }
+
+//     console.log(age);
+// }
+ 
+// let age = prompt ("nhập vào tuổi của bạn ");
+
+// nếu age nhỏ hơn 18 thì => "không cho vào "
+// nếu age lớn hơn hoặc bằng 18 => " vào thoải mái "
+
+// if (age < 18) {
+//     console.log("không cho vào");
+// } else {
+//     consolekjuy.log("vào thoải mái");
+// }
+
+// nhập vào phân loại của 1 thằng học sinh: 1, 2, 3
+// nếu phân loại là 1 => 'Thằng này xứng đáng có 10 người yêu'
+// nếu phân loại là 2 => '9,5 điểm'
+// nếu phân loại là 3 => 'cần có cố gắng hơn'
+
+// let rank = prompt('nhập vào phân loại');
+// if (rank == 1) {
+//     console.log('Thằng này xứng đáng có 10 người yêu')
+// } else if(rank == 2) {
+//     console.log('9,5 điểm');
+// } else if(rank == 3) {
+//     console.log('cần cố gắng hơn');
+// } else {
+//     console.log('nhập đúng rank của bạn');
+// }
+
+
+// phần diều kiện
+// let text = prompt('nhập vào 1 chuỗi');
+
+// if(text.length < 8) {
+//     console.log('chuỗi ngắn quá');
+// } else {
+//     console.log('chuỗi này ok');
+// }
+
+// in ra dòng "I love you" 500 lần, mỗi dòng có đánh số thứ tự 
+
+// for(let a = 1; a <= 500; a++) {
+//     console.log(a + ".I love you");
+// }
+
+// for(let a = 1; a <= 50; a++){
+//     console.log(a);
+// }
+// let num = 10;
+// while (num >= 6) {
+//     console.log(num);
+//     num = math.floor(math.random() * 10);
+// }
+
+// let index = 1;
+// while (index <= 200) {
+//     console.log(index);
+//     index++;
+// }
+
+// let s = prompt ("chuỗi S");
+// let l = prompt ('sô 1');
+
+// while (s.length < 1) {
+//     s += 'a';
+// }
+// console.log(s);
+
+// let x = prompt('nhập số X');
+// let y = prompt('nhập số y');
+
+// if(
+//     x >= 0
+//     && x < y 
+//     && y <= 100
+// ) {
+//     while (x < y) {
+//         x++;
+//         y--;
+//     }
+// }
+// console.log(x , y);
+// {
+
+// }
+// bài 1 
+// for(let a = 1; a <= 500; a++){
+//     console.log(a);
+// }
+
+
+// nhập vào sô n (2<=n<=10) in ra bản cửu chương của sô n
+
+let n = Number(prompt("nhập số n"))
+
+for (let i = 1; i <= 10; i++)
+{
+    console.log();
+}
