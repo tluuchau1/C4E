54cc10bf667ccc5ca78d4347bda8ceccf02f2829
tluuchau1 <tluuchau1@gmail.com>
2023-03-29 19:11:16 +0700

    Update app.js

--- lesson 9/app.js
+++ lesson 9/app.js
@@ -97,7 +97,7 @@ Call back
 //         console.log(this);
 //     }
 // }
-
+/*
 function timNghiem = (a, b, c) => {
     if(a == 0) return;
     
@@ -105,4 +105,50 @@ function timNghiem = (a, b, c) => {
     else {
         let arrNghiem =[];
     }
-}
+}*/
+
+// 1. Khai báo 1 Function với đầu vào là 3 số a,b,c (a khác 0). Hãy tìm nghiệm của phương trình a.x^2 + b.x + c = 0 và đưa ra kết quả dưới dạng 1 mảng gồm các nghiệm của phương trình 
+    /*
+function math(a, b, c) {
+    let delta = b * b - 4 * a * c;
+    if (delta < 0) {
+      return []; // Nếu delta âm thì phương trình vô nghiệm và trả về một mảng rỗng
+    } else if (delta === 0) {
+      let x = -b / (2 * a);
+      return [x]; // Nếu delta bằng 0 thì phương trình có nghiệm kép và trả về mảng chứa duy nhất một nghiệm
+    } else {
+      let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+      let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+      return [x1, x2]; // Nếu delta dương thì phương trình có hai nghiệm phân biệt và trả về mảng chứa hai nghiệm đó
+    }
+  }
+  let result = math(1, -5, 6); 
+    console.log(result); 
+  */
+ //2. Khai báo 1 function với đầu vào là 3 số a,b,c. kiểm tra xem a,b,c có tạo thành 3 cạnh của tam giác không. Nếu tạo thành tam giác, đầu ra là true. ngược lại,đầu ra là false.
+/*
+ function isTriangle(a, b, c) {
+    if (a + b > c && a + c > b && b + c > a) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  console.log(isTriangle(3, 4, 5)); 
+    console.log(isTriangle(1, 2, 3));
+*/
+//3. Khai báo 1 function với đầu vào là day, month, year. kiểm tra xem 3 giá trị đó có tạo thành 1 ngày, tháng, năm hợp lệ không.
+/*
+function isValidDate(day, month, year) {
+    const date = new Date(year, month - 1, day);
+    return (
+      date.getFullYear() === year &&
+      date.getMonth() + 1 === month &&
+      date.getDate() === day
+    );
+  }
+
+  console.log(isValidDate(29, 3, 2023)); 
+console.log(isValidDate(31, 2, 2021));
+*/
